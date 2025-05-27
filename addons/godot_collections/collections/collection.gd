37679a107aeeac59c861b5cf56ecc6a7b1d09380
ki11errabbit <ki11errabbit@posteo.net>
2025-05-26 17:51:07 -0700

    changed Iterator and Collection to use RefCounted

--- addons/godot_collections/collections/collection.gd
+++ addons/godot_collections/collections/collection.gd
@@ -1,4 +1,4 @@
-class_name Collection extends Object
+class_name Collection extends RefCounted
 
 
 
--- addons/godot_collections/iterator/iterator.gd
+++ addons/godot_collections/iterator/iterator.gd
@@ -1,4 +1,4 @@
-class_name Iterator extends Object
+class_name Iterator extends RefCounted
 
 func next() -> Variant:
 	return null
